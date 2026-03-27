6eeffb1173c0843976347dfb4e91b0565c72a65e
PashaTen <mamedoff1337@gmail.com>
2026-03-27 12:59:25 +0300

    Written by locator for the OK.ru

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -3,15 +3,16 @@ from selenium.webdriver.common.by import By
 
 class LoginPageLocators:
     LOGIN_FIELD = (By.ID, 'field_email')
-    LOGIN_BUTTON = (By.XPATH, '//*[@id="tabpanel-login-9186554079"]/vkid-form-adapter/div/div/div/div[1]/form/button/span')
-    LOGIN_SHOW_HIDE_PASSWORD=(By.XPATH,'//*[@id="tabpanel-login-492477137"]/vkid-form-adapter/div/div/div/div[1]/form/div[2]/span/div/div[2]/span/button')
-    LOGIN_UPPER_ENTRANCE=(By.XPATH, '//*[@id="login-9186554079"]')
-    LOGIN_TOP_QR_ENTRANCE=(By.XPATH,'//*[@id="qrCode-9186554120"]')
-    LOGIN_QR_BUTTON=(By.XPATH, '//*[@id="tabpanel-login-9186554079"]/vkid-form-adapter/div/div/div/div[1]/button/span')
-    LOGIN_FORGOT_PASSWORD=(By.XPATH,'//*[@id="tabpanel-login-9186554079"]/vkid-form-adapter/div/div/div/div[1]/span/button')
-    LOGIN_VK=(By.XPATH,'//*[@id="tabpanel-login-492477137"]/vkid-form-adapter/div/div/div/div[3]/div[4]/ui-part/div/a[1]/i')
-    LOGIN_MAIL=(By.XPATH,'//*[@id="tabpanel-login-492477137"]/vkid-form-adapter/div/div/div/div[3]/div[4]/ui-part/div/a[2]/i')
-    LOGIN_YANDEX=(By.XPATH,'//*[@id="tabpanel-login-492477137"]/vkid-form-adapter/div/div/div/div[3]/div[4]/ui-part/div/a[3]/i')
+    LOGIN_BUTTON = (By.ID, 'field_password')
+    LOGIN_SHOW_HIDE_PASSWORD=(By.CLASS_NAME,'vkuiTappable__stateLayer vkuiTappable__ripple')
+    LOGIN_UPPER_ENTRANCE=(By.XPATH, '//*[@data-l="t,login_tab"]')
+    LOGIN_TOP_QR_ENTRANCE=(By.XPATH,'//*[@data-l="t,qr_tab"]')
+    LOGIN_QR_BUTTON=(By.CLASS_NAME,'vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--qr_24')
+    LOGIN_FORGOT_PASSWORD=(By.CLASS_NAME,'vkuiInternalTappable vkuiLink__host vkuiLink__withUnderline vkuiTappable__host vkuiTappable__sizeXNone vkuiTappable__hasPointerNone vkuiClickable__host vkuiClickable__realClickable vkuistyles__-focus-visible vkuiRootComponent__host')
+    LOGIN_SIGN_UP=(By.CLASS_NAME,'vkuiInternalTappable vkuiButton__host vkuiButton__sizeL vkuiButton__modeSecondary vkuiButton__appearanceNeutral vkuiButton__sizeYNone vkuiButton__stretched vkuiTappable__host vkuiTappable__sizeXNone vkuiTappable__hasPointerNone vkuiClickable__host vkuiClickable__realClickable vkuistyles__-focus-visible vkuiRootComponent__host')
+    LOGIN_VK=(By.CLASS_NAME, 'i ic social-icon __s __vk_id')
+    LOGIN_MAIL=(By.CLASS_NAME,'i ic social-icon __s __mailru')
+    LOGIN_YANDEX=(By.CLASS_NAME,'i ic social-icon __s __yandex')
 
 
 
