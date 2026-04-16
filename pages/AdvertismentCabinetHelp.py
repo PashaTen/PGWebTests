9ea85da891cb8561776a33b5b974970474536931
PashaTen3 <mamedoff1337@gmail.com>
2026-04-17 01:51:45 +0300

    Changes class BasePage -> BasePageLocators Created VKEcosystemPage.py and GoToEcosystemTests.py

--- pages/AdvertismentCabinetHelp.py
+++ pages/AdvertismentCabinetHelp.py
@@ -1,4 +1,4 @@
-from pages.BasePage import BasePage
+from pages.BasePage import BasePageHelper
 from selenium.webdriver.common.by import By
 import allure
 
@@ -6,7 +6,7 @@ import allure
 class AdvertismentCabinetHelpLocators:
     TITLE = (By.XPATH,'//span[text()="Рекламный кабинет"]')
 
-class AdvertismentCabinetHelpPageHelper(BasePage):
+class AdvertismentCabinetHelpPageHelper(BasePageHelper):
     def __init__(self, driver):
         self.driver = driver
         self.check_page()
--- tests/HelpTests.py
+++ tests/HelpTests.py
@@ -1,7 +1,7 @@
 import allure
 
 from core.BaseTest import browser
-from pages.BasePage import BasePage
+from pages.BasePage import BasePageHelper
 from pages.HelpPage import HelpPageHelper,HelpPageLocators
 from pages.AdvertismentCabinetHelp import AdvertismentCabinetHelpPageHelper
 
@@ -10,7 +10,7 @@ BASE_URL = 'https://ok.ru/help'
 @allure.suite('Проверка страницы Помощь')
 @allure.title('Проверка скролла и перехода на страницу Рекламный кабинет')
 def test_help_test(browser):
-    BasePage(browser).get_url(BASE_URL)
+    BasePageHelper(browser).get_url(BASE_URL)
     HelpPage = HelpPageHelper(browser)
     HelpPage.scrollToitem(HelpPageLocators.ADVERTISMENT_CABINET)
     AdvertismentCabinetHelpPageHelper(browser)
